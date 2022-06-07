69e87611662dd5f04c1d8d61e5c5d4a3676af7c8
kelvin-kariuki <Kariuki405@gmail.com>
2022-06-07 16:32:28 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+console.log(multiply);
+let random = Math.floor( Math.random() * 10 ) + 1;
+console.log(random);
+let num3 = 16;
+let num4 = 6;
+
+let mod = 16%6;
+console.log(mod);
+let max = Math.max(15,16,15,14,1,7,20);
+
+console.log(max);
+
+
